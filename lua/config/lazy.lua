45905b4234fde1c4f76f8d7db79bbb45c597ecac
Corentin <coseznec@proton.me>
2025-10-16 22:48:10 +0200

    reorganize plugins

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -24,47 +24,13 @@ vim.opt.rtp:prepend(lazypath)
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
-local plugins = {
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-  {"folke/tokyonight.nvim", lazy = false, priority = 1000, opts = {}},
-  {
-  'nvim-treesitter/nvim-treesitter',
-  lazy = false,
-  branch = 'main',
-  build = ':TSUpdate'
-  },
-  { require("plugins.config_telescope")},
-  {
-    'MeanderingProgrammer/render-markdown.nvim',
-    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.nvim' },            -- if you use the mini.nvim suite
-    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.icons' },        -- if you use standalone mini plugins
-    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
-    ---@module 'render-markdown'
-    ---@type render.md.UserConfig
-    opts = {},
-}
-}
-
-local opts = {}
-
 -- Setup lazy.nvim
 require("lazy").setup({
   spec = {
-    plugins, opts
+      { import = "plugins" },
   },
-})
-require('render-markdown').setup({
-  heading = {
-    width = { 'block'},
-    sign = true,
-    left_margin = 10,
-  },
-})
-require("catppuccin").setup()
-vim.cmd.colorscheme "catppuccin-mocha" --catppuccin-frappe" --"tokyonight" "catppuccin" -- catppuccin-latte, catppuccin-macchiato 
+  })
 
-require'nvim-treesitter'.setup {
-  -- Directory to install parsers and queries to
-  install_dir = vim.fn.stdpath('data') .. '/site'
-}
+
+
 
---
+++ lua/plugins/treesitters.lua
@@ -0,0 +1,12 @@
+return {
+  'nvim-treesitter/nvim-treesitter',
+  lazy = false,
+  branch = 'main',
+  build = ':TSUpdate',
+  config = function ()
+    require('nvim-treesitter').setup({
+    -- Directory to install parsers and queries to
+    install_dir = vim.fn.stdpath('data') .. '/site'
+    })
+  end,
+}
