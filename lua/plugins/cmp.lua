72bb3cd2b994e11a46fa2d3684f73ea86a9d802d
Corentin <coseznec@proton.me>
2025-10-19 11:06:45 +0200

    cmp.lua

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -24,16 +24,17 @@ return {
       local cmp = require'cmp'
       require("luasnip.loaders.from_vscode").lazy_load()
       -- Optionnel : mappages pour naviguer dans les snippets
-      vim.keymap.set({ "i", "s" }, "<C-k>", function()
-        if ls.expand_or_jumpable() then
-          ls.expand_or_jump()
+      local ls = require("luasnip")
+
+      vim.keymap.set({"i"}, "<CR>", function() ls.expand() end, {silent = true})
+      vim.keymap.set({"i", "s"}, "<Tab>", function() ls.jump( 1) end, {silent = true})
+      vim.keymap.set({"i", "s"}, "<S-Tab>", function() ls.jump(-1) end, {silent = true})
+
+      vim.keymap.set({"i", "s"}, "<C-E>", function()
+        if ls.choice_active() then
+          ls.change_choice(1)
         end
-      end)
-      vim.keymap.set({ "i", "s" }, "<C-j>", function()
-        if ls.jumpable(-1) then
-          ls.jump(-1)
-        end
-      end)
+      end, {silent = true})
 
       ls.add_snippets("python", {
        s("bbb", {
