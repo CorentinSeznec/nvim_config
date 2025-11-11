0b0b9aa19e23b248dc7a15bdc2ebfc8fc17ee745
SEZNEC Corentin <seznecc@px0112t317>
2025-11-11 17:04:14 +0100

    treesitters correction

--- lua/plugins/treesitters.lua
+++ lua/plugins/treesitters.lua
@@ -3,16 +3,5 @@ return {
   lazy = false,
   branch = 'main',
   build = ':TSUpdate',
-  config = function()
-    require('nvim-treesitter.configs').setup({
-      ensure_installed = { "python" },   -- Installe le parser Python
-      highlight = {
-        enable = true,                   -- Active la coloration Treesitter
-        -- additional_vim_regex_highlighting = false,  -- Désactive la coloration classique
-      },
-      -- Optional: choisir un dossier différent pour les parsers
-      install_dir = vim.fn.stdpath('data') .. '/site',
-    })
-  end,
 }
 
