2b1d81c504a20e3dcfe49e0a5a3ee567ea56ef29
Corentin <coseznec@proton.me>
2026-03-19 16:38:01 +0100

    telespope enter in initial mode

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -9,39 +9,40 @@ return {
   },
   config = function()
     local telescope = require("telescope")
-    local fb_actions = telescope.extensions.file_browser.actions
+    -- local fb_actions = telescope.extensions.file_browser.actions
     telescope.setup({
       defaults = {
         layout_strategy = "flex",
+        initial_mode = "normal",
         -- sorting_strategy = "ascending",
       },
-      extensions = {
-        file_browser = {
-          initial_mode = "normal",
-          mappings = {
-            ["i"] = {
-              ["<A-c>"] = fb_actions.create,
-              ["<A-t>"] = fb_actions.rename,
-              ["<C-e>"] = fb_actions.goto_home_dir,
-              ["<C-g>"] = fb_actions.goto_parent_dir,
-            },
-            ["n"] = {
-              ["c"] = fb_actions.create,
-              ["r"] = fb_actions.rename,
-              ["e"] = fb_actions.goto_home_dir,
-              ["g"] = fb_actions.goto_parent_dir,
-            },
-          },
-        },
-      },
+      -- extensions = {
+      --   file_browser = {
+      --     initial_mode = "normal",
+      --     mappings = {
+      --       ["i"] = {
+      --         ["<A-c>"] = fb_actions.create,
+      --         ["<A-t>"] = fb_actions.rename,
+      --         ["<C-e>"] = fb_actions.goto_home_dir,
+      --         ["<C-g>"] = fb_actions.goto_parent_dir,
+      --       },
+      --       ["n"] = {
+      --         ["c"] = fb_actions.create,
+      --         ["r"] = fb_actions.rename,
+      --         ["e"] = fb_actions.goto_home_dir,
+      --         ["g"] = fb_actions.goto_parent_dir,
+      --       },
+      --     },
+      --   },
+      -- },
     })
 
-    require("telescope").load_extension "file_browser"
+    -- require("telescope").load_extension "file_browser"
     local builtin = require('telescope.builtin')
     vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
     vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
     vim.keymap.set('n', '<leader>fp', builtin.buffers, { desc = 'Telescope buffers' })
     vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
     
-  end
+ end
 }
